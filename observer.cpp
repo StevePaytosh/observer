6495935e7d121962c684078c71309c2f17bfcb87
Steve <mrblond619f5@hotmail.com>
2017-05-07 03:58:10 +0000

    commentary on page will help guide further development

---
+++ observer.cpp
@@ -0,0 +1,27 @@
+/* 
+ * Observers primary function will be to monitor
+ * tempeture conditions in a room, display that data to
+ * a screen and store that data in a database.
+ * 
+ * 
+ * The program is intended to be run on a raspberry pi or similar microcontroller
+ * 
+ * When tempetures exceed a minimum threshold, the program
+ * should send out an email to relevant parties the room has become to
+ * hot. The email should be repeated only once per hour while the room
+ * is still too hot.
+ * 
+ * Similar behaivor should be enabled for cold tempetures.
+ * 
+ * database storage and emails should be handled
+ * as API requests in order to minimize stress on the unit hosting this program
+ * 
+ * 
+ * 
+ * */
+
+
+int main()
+{
+	
+}
---
+++ observer.h
@@ -0,0 +1,8 @@
+float temp;
+time_t last_email_sent;
+
+void sendEmail();
+void writeToScreen();
+void readTemp();
+void updateDB();
+
