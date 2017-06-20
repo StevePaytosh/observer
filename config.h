235148fbe79561c882877338029da68a1f0400a3
Steve <mrblond619f5@hotmail.com>
2017-06-20 02:04:52 +0000

    created basic config file

---
+++ config.h
@@ -0,0 +1,42 @@
+//LEFT SEGMENT DISPLAY
+#define LEFT_SEG_TOP_LEFT "27"
+#define LEFT_SEG_TOP "15"
+#define LEFT_SEG_TOP_RIGHT "17"
+#define LEFT_SEG_BOTTOM_RIGHT "14"
+#define LEFT_SEG_BOTTOM "22"
+#define LEFT_SEG_BOTTOM_LEFT "2"
+#define LEFT_SEG_MIDDLE "3"
+#define LEFT_SEG_DOT "18"
+#define LEFT_SEG_SANS "false"
+#define COMMON "cathode"
+
+//RIGHT SEGMENT DISPLAY
+#define RIGHT_SEG_TOP_LEFT "10"
+#define RIGHT_SEG_TOP "24"
+#define RIGHT_SEG_TOP_RIGHT "23"
+#define RIGHT_SEG_BOTTOM_RIGHT "8"
+#define RIGHT_SEG_BOTTOM "7"
+#define RIGHT_SEG_BOTTOM_LEFT "11"
+#define RIGHT_SEG_MIDDLE "9"
+#define RIGHT_SEG_DOT "5"
+#define RIGHT_SEG_SANS "true"
+#define COMMON "cathode"
+
+//RED GUMDROP
+#define RED_GUMDROP "20"
+
+//BLUE GUMDROP
+#define BLUE_GUMDROP "21"
+
+
+//TACTILE SWITCH
+#define TACT_SWITCH "26"
+#define ACTIVE "low"
+
+//TEMPERATURE SENSOR
+#define DTH11PIN
+
+//SAVE FILES
+#define TEMP "/home/pi/Desktop/code/git/observer/save.dat"
+
+
